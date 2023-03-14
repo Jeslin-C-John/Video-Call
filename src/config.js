da8a60200417facfabc3b8b1a1fc7e282ab8e8a3
Fredric Jose <Fredric.Jose@oneteam.us>
2023-03-14 12:57:24 +0530

    port changes

--- src/config.js
+++ src/config.js
@@ -1,27 +1,27 @@
-const os = require('os')
-const ifaces = os.networkInterfaces()
+const os = require("os");
+const ifaces = os.networkInterfaces();
 
 const getLocalIp = () => {
-  let localIp = '127.0.0.1'
+  let localIp = "127.0.0.1";
   Object.keys(ifaces).forEach((ifname) => {
     for (const iface of ifaces[ifname]) {
       // Ignore IPv6 and 127.0.0.1
-      if (iface.family !== 'IPv4' || iface.internal !== false) {
-        continue
+      if (iface.family !== "IPv4" || iface.internal !== false) {
+        continue;
       }
       // Set the local ip to the first IPv4 address found and exit the loop
-      localIp = iface.address
-      return
+      localIp = iface.address;
+      return;
     }
-  })
-  return localIp
-}
+  });
+  return localIp;
+};
 
 module.exports = {
-  listenIp: '127.0.0.1',
-  listenPort: 3016,
-  sslCrt: '../ssl/cert.pem',
-  sslKey: '../ssl/key.pem',
+  listenIp: "127.0.0.1",
+  listenPort: 4200,
+  sslCrt: "../ssl/cert.pem",
+  sslKey: "../ssl/key.pem",
 
   mediasoup: {
     // Worker settings
@@ -29,50 +29,50 @@ module.exports = {
     worker: {
       rtcMinPort: 10000,
       rtcMaxPort: 10100,
-      logLevel: 'warn',
+      logLevel: "warn",
       logTags: [
-        'info',
-        'ice',
-        'dtls',
-        'rtp',
-        'srtp',
-        'rtcp'
+        "info",
+        "ice",
+        "dtls",
+        "rtp",
+        "srtp",
+        "rtcp",
         // 'rtx',
         // 'bwe',
         // 'score',
         // 'simulcast',
         // 'svc'
-      ]
+      ],
     },
     // Router settings
     router: {
       mediaCodecs: [
         {
-          kind: 'audio',
-          mimeType: 'audio/opus',
+          kind: "audio",
+          mimeType: "audio/opus",
           clockRate: 48000,
-          channels: 2
+          channels: 2,
         },
         {
-          kind: 'video',
-          mimeType: 'video/VP8',
+          kind: "video",
+          mimeType: "video/VP8",
           clockRate: 90000,
           parameters: {
-            'x-google-start-bitrate': 1000
-          }
-        }
-      ]
+            "x-google-start-bitrate": 1000,
+          },
+        },
+      ],
     },
     // WebRtcTransport settings
     webRtcTransport: {
       listenIps: [
         {
-          ip: '0.0.0.0',
-          announcedIp: getLocalIp() // replace by public IP address
-        }
+          ip: "0.0.0.0",
+          announcedIp: getLocalIp(), // replace by public IP address
+        },
       ],
       maxIncomingBitrate: 1500000,
-      initialAvailableOutgoingBitrate: 1000000
-    }
-  }
-}
+      initialAvailableOutgoingBitrate: 1000000,
+    },
+  },
+};
